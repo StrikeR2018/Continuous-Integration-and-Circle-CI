47d8a5140f15859e17d5510b37a433b47ccda59f
Mingxuan Li <52895636+StrikeR2018@users.noreply.github.com>
2021-06-08 02:29:21 -0700

    upload test file

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+"""
+Tests for calculator app
+"""
+import calculator
+
+
+class TestCalculatorAPP:
+    def test_add(self):
+        assert 7 == calculator.add(3, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(4, 6)
+
+    def test_mul(self):
+        assert 12 == calculator.mulityply(3, 4)
+
+    def test_div(self):
+        assert 2 == calculator.divide(16, 8)
